62e172e0c2a6b77bad66db4fb153347e2682a026
brokenfloppydisk <kylehuang06@gmail.com>
2022-03-14 13:24:26 -0700

    Add quotes

--- quotes.py
+++ quotes.py
@@ -1,4 +1,8 @@
+# List of quotes to use for random ciphers
+# Quote 2, quote 3, and quote 4 are from the apstudents description of AP CSP in the digital portfolio.
 quotes = [
-    "This is a test",
-    "This is another test"
+    "AP Computer Science Principles is an amazing class!",
+    "AP Computer Science Principles is an introductory college-level computing course that introduces students to the breadth of the field of computer science.",
+    "Students learn to design and evaluate solutions and to apply computer science to solve problems through the development of algorithms and programs.",
+    "Students also explain how computing innovations and computing systems—including the internet—work, explore their potential impacts, and contribute to a computing culture that is collaborative and ethical."
 ]
