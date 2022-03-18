f9b23e5c8fd0f0654e69e4b96d63e28a24feaba8
brokenfloppydisk <kylehuang06@gmail.com>
2022-03-18 12:32:54 -0700

    Reformat quotes to be more readable

--- quotes.py
+++ quotes.py
@@ -2,7 +2,13 @@
 # Quote 2, quote 3, and quote 4 are from the apstudents description of AP CSP in the digital portfolio.
 quotes = [
     "AP Computer Science Principles is an amazing class!",
-    "AP Computer Science Principles is an introductory college-level computing course that introduces students to the breadth of the field of computer science.",
-    "Students learn to design and evaluate solutions and to apply computer science to solve problems through the development of algorithms and programs.",
-    "Students also explain how computing innovations and computing systems—including the internet—work, explore their potential impacts, and contribute to a computing culture that is collaborative and ethical."
+    "AP Computer Science Principles is an introductory" + 
+        "college-level computing course that introduces students " +
+        "to the breadth of the field of computer science.",
+    "Students learn to design and evaluate solutions and " +
+        "to apply computer science to solve problems through " +
+        "the development of algorithms and programs.",
+    "Students also explain how computing innovations and computing " +
+        "systems—including the internet—work, explore their potential " +
+        "impacts, and contribute to a computing culture that is collaborative and ethical."
 ]
