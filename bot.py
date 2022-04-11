9352f8a3df16bb6c74458ea62066a06eb4e93329
brokenfloppydisk <kylehuang06@gmail.com>
2022-04-11 14:00:42 -0700

    Start working on basic functions

--- bot.py
+++ bot.py
@@ -1,12 +1,32 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 
-import ciphers.aristocrat as aristo
-import ciphers.alphabet as alpha
-import ciphers.quotes as quotes
-from ciphers.alphabet import Alphabet
 
-bot = commands.Bot(command_prefix=["c! "], description="Cipher Bot")
+class cipher_bot(commands.Bot):
+
+    ciphers_dict = {
+        "aristocrat" : (lambda : ())
+    }
+
+    def __init__(self):
+        self.command_prefix = "!"
+        self.cipher_context: commands.Context = None
+
+    @commands.command()
+    async def cipher(self, ctx: commands.Context, args):
+        """ Start solving a cipher
+        """
+        self.cipher_context = ctx
+
+        if args in self.ciphers_dict:
+            await self.ciphers_dict[args]()
+    
+    @commands.command()
+    async def prefix(self, ctx: commands.Context, args):
+        await ctx.channel.send(f"Prefix changed to `{str(args)}`!")
+        self.command_prefix = str(args)
+
+bot = cipher_bot(command_prefix=["c! "], description="Cipher Bot")
 
 # Get bot token
 with open("token.txt", "r") as t:
