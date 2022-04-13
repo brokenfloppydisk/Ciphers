d66235fa1bfc53c1da4a9447d1c2faee67d362bd
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2022-04-12 23:31:45 -0700

    Add fetch_message() to bot class

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import asyncio
+from pyclbr import Function
+from tokenize import Double
 import nextcord
 from nextcord.ext import commands
 from commands.default_commands import DefaultCommands
@@ -7,14 +10,50 @@ class cipher_bot(commands.Bot):
         "aristocrat" : (lambda : ())
     }
 
-    def __init__(self, command_prefix, help_command=None, description=None, self_bot=False):
-        commands.Bot.__init__(self, command_prefix=command_prefix, help_command=help_command, description=description, self_bot=self_bot)
+    def __init__(self, command_prefix, help_command=None, description=None, self_bot=False) -> None:
+        """ Initialize the bot. Calls bot.__init__()
+        """
+        commands.Bot.__init__(self, command_prefix=command_prefix, help_command=help_command, 
+                              description=description, self_bot=self_bot)
+        
         self.cipher_context: commands.Context = None
+
+        self.most_recent_message: nextcord.Message = None
+        self.new_content: bool = False
+
         self.add_cog(DefaultCommands(self))
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
+        """ Runs when the bot initializes.
+        """
         print(f"{self.user.name} initialized")
         print(f"ID: {self.user.id}")
+    
+    async def fetch_message(self, author: nextcord.abc.User, check_rate: Double=0.5) -> nextcord.Message:
+        """ Fetch the next message that has the same author.
+        """
+        while not self.new_content and self.most_recent_message.author == author:
+            await asyncio.sleep(check_rate)
+        self.new_content = False
+        return self.most_recent_message
+
+    async def fetch_message(self, condition: Function, check_rate: Double=0.5) -> nextcord.Message:
+        """ Fetch the next message that matches the condition
+        """
+        while not self.new_content and condition():
+            await asyncio.sleep(check_rate)
+        self.new_content = False
+        return self.most_recent_message
+
+    async def on_message(self, message: nextcord.Message) -> None:
+        """ Called every time a message is sent
+        """
+        # Set as the most recent message for fetch_message()
+        self.new_content = True
+        self.most_recent_message = message
+
+        # Run all commands
+        await self.process_commands(message)
 
 bot = cipher_bot(command_prefix=["!"], description="Cipher Bot")
 
